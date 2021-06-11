15c51d53aa6715acd742bf84b9e7cc482c4ad01e
Pavan Singhal <Pavan.Singhal@smartcontrols.in>
2021-06-11 17:13:34 +0530

    changes for jdbc template

--- src/main/java/com/firstapp/crudoperation/CrudoperationApplication.java
+++ src/main/java/com/firstapp/crudoperation/CrudoperationApplication.java
@@ -1,13 +1,32 @@
 package com.firstapp.crudoperation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import com.firstapp.crudoperation.models.WCMaster;
 
 @SpringBootApplication
-public class CrudoperationApplication {
+public class CrudoperationApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudoperationApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "SELECT * FROM dbo.TBM_WCMaster";
+
+		List<WCMaster> wcMasters = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(WCMaster.class));
+
+		wcMasters.forEach(System.out::println);
 	}
 
 }
---
+++ src/main/java/com/firstapp/crudoperation/models/WCMaster.java
@@ -0,0 +1,38 @@
+package com.firstapp.crudoperation.models;
+
+public class WCMaster {
+
+    private int id;
+    private String name;
+    private int area;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "WCMaster [area=" + area + ", id=" + id + ", name=" + name + "]";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+}
